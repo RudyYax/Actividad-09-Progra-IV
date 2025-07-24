89798421349a936bc4ea3d123c6fea1cbc975b13
Rudy <rudyax2004@gmail.com>
2025-07-23 18:58:54 -0600

    commit inicial

--- actividad9.py
+++ actividad9.py
@@ -8,8 +8,8 @@ while opcion != 3:
     print("2.- Mostrar listado")
     print("3.- Salir")
     opcion = int(input("Ingrese la opcion que deseea"))
-    match opcion:
-        case "1":
+    match (opcion):
+        case 1:
             print("\n Registro clientes")
             registro = int(input("Ingrese la cantidad de clientes desea registrar: "))
             for i in range(registro):
@@ -17,11 +17,15 @@ while opcion != 3:
                 codigo = input("Ingrese el codigo del cliente: ")
                 Clientes[codigo] = {}
                 Clientes[codigo]["nombre"] = input("Ingrese el nombre del cliente: ")
-                Cantidad_lugares = input("Ingrese la cantidad de lugares desea registrar (Max 5: ")
-                if Cantidad_lugares > "5":
-                    print("No pueden ser mas de 5 lugares ")
-                else:
+                Cantidad_lugares = int(input("Ingrese la cantidad de lugares desea registrar (Max 5: "))
+                if Cantidad_lugares <= 5:
                     for f in range(Cantidad_lugares):
+                        lugar = input(f"Ingrese el {f+1} lugar del cliente: ")
+                        Clientes[codigo]["lugar"] = {
+                            "Lugar": lugar,
+                        }
+                else:
+                    print("No se puede mas de 5 lugares ")
 
 
 
