6f53c1b9a12f6155a631c59851dadbcbd66acd04
Rudy <rudyax2004@gmail.com>
2025-07-23 18:44:36 -0600

    commit inicial

--- actividad9.py
+++ actividad9.py
@@ -1,1 +1,16 @@
-print("Bienvendios Actividad 09")
+Clientes = {}
+opcion = 0
+while opcion != 3:
+    print("Bienvendios Actividad 09")
+    print("Bienvenido agencia de viajes")
+    print("Destinos turisticos visitados por clientes")
+    print("1.- Registro clientes")
+    print("2.- Mostrar listado")
+    print("3.- Salir")
+    opcion = int(input("Ingrese la opcion que deseea"))
+    match opcion:
+        case "1":
+            print("Registro clientes")
+            registro = int(input("Ingrese la cantidad de clientes desea registrar: "))
+
+
